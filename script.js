f22240e25d2108457a8433b0150815450b419689
varun-1001 <kumawatvarun@asmedu.org>
2025-12-18 14:57:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbarToogler = document.querySelector(".navbar-toogler");
+
+navbarToogler.addEventListener("click", () => {
+
+    console.log();
+    navbarToogler.classList.toggle('active');
+
+});
