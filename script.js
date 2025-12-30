d9d159c3d0d2028f8614422b4ef20414a997f3ca
varun-1001 <kumawatvarun@asmedu.org>
2025-12-30 15:34:43 +0530

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
-const navbarToogler = document.querySelector(".navbar-toogler");
+$("document").ready(() => {
+    $(".testimonials-slider").slick(
+        {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            centerMode: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            centerPadding: "1px",
+            asNavFor: '.testimonials-content'
+        }
+    );
+    $(".testimonials-content").slick(
+        {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            asNavFor: '.testimonials-slider'
+        }
+    );
+    $('.testimonials-slider .slick-current').next().addClass("sl-next");
+    $('.testimonials-slider .slick-current').prev().addClass("sl-prev");
 
-navbarToogler.addEventListener("click", () => {
-
-    console.log();
-    navbarToogler.classList.toggle('active');
-
+    $(".testimonials-slider").on(
+        "beforeChange",
+        function (event, slick, currentslide, nextslide) {
+            $(".testimonials-slider")
+                .find(".slick-current")
+                .next()
+                .removeClass("sl-next");
+            $(".testimonials-slider")
+                .find(".slick-current")
+                .prev()
+                .removeClass("sl-prev");
+        }
+    )
+    $(".testimonials-slider").on(
+        "afterChange",
+        function (event, slick, currentslide, nextslide) {
+            $(".testimonials-slider")
+                .find(".slick-current")
+                .next()
+                .addClass("sl-next");
+            $(".testimonials-slider")
+                .find(".slick-current")
+                .prev()
+                .addClass("sl-prev");
+        }
+    )
 });
